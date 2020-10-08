a574e2e5d59cf8008ee3664fa34a05140acc7baa
José Eduardo Méndez Barrios <joseeduardomendezb@gmail.com>
2020-10-08 02:47:56 -0700

    add list component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div class="columns">
-      <div class="column full-with">
+      <div class="column is-full">
         <h1>Filters & Mixings</h1>
         <p>{{ text | toUppercase | toLowercase }}</p>
         <hr />
@@ -9,12 +9,15 @@
         <ul>
           <li v-for="fruit in filteredFruits" :key="fruit">{{ fruit }}</li>
         </ul>
+        <hr />
+        <app-list></app-list>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import List from './List'
 export default {
   data () {
     return {
@@ -34,17 +37,11 @@ export default {
         return element.match(this.filterText)
       })
     }
+  },
+  components: {
+    appList: List
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/List.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="column is-full">
+    <h1>Filters & Mixings</h1>
+    <hr />
+    <input type="text" v-model="filterText" />
+    <ul>
+      <li v-for="fruit in filteredFruits" :key="fruit">{{ fruit }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      fruits: ['Apple', 'Banana', 'Mango', 'Melon'],
+      filterText: ''
+    }
+  },
+  computed: {
+    filteredFruits () {
+      return this.fruits.filter(element => {
+        return element.match(this.filterText)
+      })
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
