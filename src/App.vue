d839993b236063f768a19b142f4a618e40f4e3ef
José Eduardo Méndez Barrios <joseeduardomendezb@gmail.com>
2020-10-12 04:29:46 -0700

    assigment finished

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,62 @@
     <div class="columns">
       <div class="column">
         <h1>Filters & Mixins</h1>
-        <!-- Exercise 1) -->
-        <!-- Build a local Filter which reverses the Text it is applied on -->
-
-        <!-- Exercise 2 -->
-        <!-- Build a global Filter which counts the length of a word and it appends it -->
-        <!-- Like this: "Test" => Gets Filtered to => "Test (4)" -->
-
-        <!-- Exercise 3 -->
-        <!-- Do the same as in Exercises 1 & 2, now with Computed Properties -->
-
-        <!-- Exercise 4 -->
-        <!-- Share the Computed Property rebuilding Exercise 2 via a Mixin -->
+        <div class="content">
+          <ol>
+            <!-- Exercise 1) -->
+            <!-- Build a local Filter which reverses the Text it is applied on -->
+            <li>{{ text | reverseText }}</li>
+            <!-- Exercise 2 -->
+            <!-- Build a global Filter which counts the length of a word and it appends it -->
+            <!-- Like this: "Test" => Gets Filtered to => "Test (4)" -->
+            <li>{{ text | countAppend }}</li>
+            <!-- Exercise 3 -->
+            <!-- Do the same as in Exercises 1 & 2, now with Computed Properties -->
+            <li>{{ reversedAndCountedText }}</li>
+            <!-- Exercise 4 -->
+            <!-- Share the Computed Property rebuilding Exercise 2 via a Mixin -->
+            <li>{{ countAndAppend }}</li>
+          </ol>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  mixins: [
+    {
+      computed: {
+        countAndAppend () {
+          return `${this.text} ${this.text.length}`
+        }
+      }
+    }
+  ],
+  data () {
+    return {
+      text: 'Hello there!'
+    }
+  },
+  filters: {
+    reverseText (value) {
+      return value
+        .split('')
+        .reverse()
+        .join('')
+    }
+  },
+  computed: {
+    reversedAndCountedText () {
+      let reversed = this.text
+        .split('')
+        .reverse()
+        .join('')
+      return `${reversed} (${reversed.length})`
+    }
+  }
+}
 </script>
 
 <style></style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import App from './App.vue'
 require('@/assets/main.scss')
 Vue.config.productionTip = false
 
+Vue.filter('countAppend', value => {
+  return `${value} (${value.length})`
+})
 new Vue({
   render: h => h(App)
 }).$mount('#app')
